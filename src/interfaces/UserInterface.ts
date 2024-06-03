8f2f7f8c8091a3c2f8083b6a972ba3a2039e793e
Ewerton Igor <ewertonigor@icloud.com>
2024-06-03 18:34:34 -0300

    adding the implementation of sending the encrypted password in the database

--- src/interfaces/UserInterface.ts
+++ src/interfaces/UserInterface.ts
@@ -3,6 +3,6 @@ import { ItemInterface } from './ItemInterface'
 export interface UserInterface {
   id?: string
   user: string
-  password: string
+  password_hash: string
   items?: ItemInterface[]
 }
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,15 +1,17 @@
 import { prisma } from '../server'
 import { UserInterface } from '../interfaces/UserInterface'
+import { hash } from 'bcryptjs'
 
 const userService = {
   createUser: async (
     user: string,
     password: string,
   ): Promise<UserInterface> => {
+    const password_hash = await hash(password, 6)
     const newUser = await prisma.user.create({
       data: {
         user,
-        password,
+        password_hash,
       },
     })
 
